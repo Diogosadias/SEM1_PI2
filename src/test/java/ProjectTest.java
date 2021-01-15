b1c3f54f2c9e8377113cf357fc09fa032ecc81d0
torrespedro19 <1191526@isep.ipp.pt>
2021-01-15 16:54:21 +0000

    Beginning of the tests if the methods

--- src/test/java/ProjectTest.java
+++ src/test/java/ProjectTest.java
@@ -1,20 +1,80 @@
-import static org.junit.jupiter.api.Assertions.*;
+package src;
+
+import org.junit.jupiter.api.Test;
+
 
 class ProjectTest {
 
-    @org.junit.jupiter.api.Test
+    @Test
     void leslieMatrix() {
+
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void convertToMatrix() {
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
+    void getPopulationfromFile() {
+    }
+
+    @Test
+    void matrixWriteFile() {
+    }
+
+    @Test
     void leslieMatrixFile() {
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
+    void eigen_value() {
+     /*   double[][] matrix_leslie = {{0, 3.5, 1.5, 0.39},
+                                    {0.4, 0, 0, 0},
+                                    {0, 0.6, 0, 0},
+                                    {0, 0, 0.5, 0}};
+
+        maxEigenvalue = eigen_value(matrix_leslie);
+            if(expectedMaxEigenValue==maxEigenvalue)
+                return true;
+            else
+                return false;
+        }*/
+
+    }
+
+    @Test
     void dimPopulationinT() {
+    }
+
+    @Test
+    void totaldimPopulation() {
+    }
+
+    @Test
+    void distriPopulation() {
+    }
+
+    @Test
+    void rateofchange() {
+    }
+
+    @Test
+    void createGraph() {
+    }
+
+    @Test
+    void gnuplotGraph() {
+    }
+
+    @Test
+    void startingGnuplot() {
+    }
+
+    @Test
+    void creatingDataFile() {
+    }
+
+    @Test
+    void creatingTxtFileGraph() {
     }
 }
